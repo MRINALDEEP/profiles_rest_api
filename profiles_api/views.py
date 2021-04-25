27761016860e74bf5f6f49adb7f4fae3d30d7ba9
mrinaldeepmanna <mrinaldeepmanna@gmail.com>
2021-04-25 16:57:55 -0700

    added viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
+from rest_framework import viewsets
 # Create your views here.
 
 class HelloAPIView(APIView):
@@ -33,4 +34,36 @@ class HelloAPIView(APIView):
         return Response({'methos':'PATCH'})
     def delete(self,request,pk=None):
         """Deletes Objects"""
+        return Response({'method':'DELETE'})
+class HelloViewSets(viewsets.ViewSet):
+    """Test API ViewSets"""
+    serializer_class = serializers.HelloSerializer
+    def list(self,request):
+        """Returns a hello message"""
+        a_viewset=[
+            'uses actions- list,create,retrieve,update,partial_update,destroy',
+            'Automatically maps URLS using routers',
+            'Provides more functinality with less code'
+        ]
+        return Response({'message':'Hello','a_viewset':a_viewset})
+    def create(self,request):
+        """Creates a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def retrieve(self,request,pk=None):
+        """Handle getting an object by its id"""
+        return Response({'method':'GET'})
+    def update(self,request,pk=None):
+        """Handle Updating a object"""
+        return Response({'method':'PUT'})
+    def partial_update(self,request,pk=None):
+        """Handle partially updating an object"""
+        return Response({'method':'PATCH'})
+    def destroy(self,request,pk=None):
+        """Handle deleteing an object"""
         return Response({'method':'DELETE'})
